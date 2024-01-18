771e7cac8426afdbc2569933c659ca23a37d73b3
orillad <orillad2003@gmail.com>
2024-01-18 18:44:56 +0100

    pinia 3

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -15,15 +15,25 @@ export const useCartStore = defineStore("CartStore", {
                     state.items.push({ ...product });
                 }
             })
-
-
+        },
+        clearItem(name) {
+            this.$patch((state) => {
+                state.items = state.items.filter((item) => item.name !== name);
+            });
+        },
+        setItemCount(item, count) {
+            this.addToCart(count, item);
+            this.clearItem(item)
         }
     },
     getters: {
         count: (state) => state.items.length,
-
         isEmpty: (state) => state.count === 0,
-        grouped: state => groupBy(state.items, item => item.name)
+        grouped: state => groupBy(state.items, item => item.name),
+        groupCount: (state) => (name) => state.grouped[name].length,
+        total() {
+            return this.items.reduce((total, items) => total + (items.price || 0), 0)
+        },
 
     }
 })
